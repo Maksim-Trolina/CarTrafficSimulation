3e2ddb2e575a926577c7cc26a838df020d23bf7a
PanasenkoMaksim <maks.panasenko01@bk.ru>
2021-06-04 01:30:26 +0300

    Init

---
+++ TrafficJam/TrafficJam/InputForm.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace TrafficJam
+{
+    partial class InputForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.shareOfCarsNumeric = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.shareOfCarsNumeric)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(32, 54);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(91, 34);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Старт";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // shareOfCarsNumeric
+            // 
+            this.shareOfCarsNumeric.DecimalPlaces = 3;
+            this.shareOfCarsNumeric.Location = new System.Drawing.Point(218, 54);
+            this.shareOfCarsNumeric.Name = "shareOfCarsNumeric";
+            this.shareOfCarsNumeric.Size = new System.Drawing.Size(99, 23);
+            this.shareOfCarsNumeric.TabIndex = 1;
+            // 
+            // InputForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.shareOfCarsNumeric);
+            this.Controls.Add(this.btnStart);
+            this.Name = "InputForm";
+            this.Text = "InputForm";
+            ((System.ComponentModel.ISupportInitialize)(this.shareOfCarsNumeric)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.NumericUpDown shareOfCarsNumeric;
+    }
+}
